66021e4f5771bd15ef7e1eea9e82aded8f5d1551
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2019-07-19 20:15:00 -0500

    trying out repl and defining functions I thought the repl would refresh automatically but it doesn't seem to.

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+
+(defn makeAFunc [a]
+  (println a))
+
+(defn call-a-func []
+  (makeAFunc "heeeeey"))
+
+(call-a-func)
